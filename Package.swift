adb24caa626815d1fa938deb44e0193bc495f38f
Adam Fowler <adamfowler71@gmail.com>
2024-09-18 10:40:03 +0100

    Add dummy package to stop it complaining

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "dummy",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "dummy",
+            targets: ["dummy"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "dummy"),
+        .testTarget(
+            name: "dummyTests",
+            dependencies: ["dummy"]
+        ),
+    ]
+)
